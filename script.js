17abbd97cc725ea01a9d15974e8ce807f4e44dde
Tymofii Mazurenko <tymofii.mazurenko@student.griffith.ie>
2025-05-02 22:28:20 +0100

    added homepage

--- script.js
+++ script.js
@@ -24,3 +24,53 @@ for (let i = 0; i < accordionBtns.length; i++) {
         }
     })
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector(".carousel");
+    const arrowBtns = document.querySelectorAll(".carousel-action-button");
+
+    const firstCard = carousel.querySelector(".card");
+    const firstCardWidth = firstCard.offsetWidth;
+
+    let isDragging = false,
+        startX,
+        startScrollLeft;
+
+    const dragStart = (e) => { 
+        isDragging = true;
+        carousel.classList.add("dragging");
+        startX = e.pageX;
+        startScrollLeft = carousel.scrollLeft;
+    };
+
+    const dragging = (e) => {
+        if (!isDragging) return;
+    
+        const newScrollLeft = startScrollLeft - (e.pageX - startX);
+
+        if (newScrollLeft <= 0 || newScrollLeft >= 
+            carousel.scrollWidth - carousel.offsetWidth) {
+            isDragging = false;
+            return;
+        }
+
+        carousel.scrollLeft = newScrollLeft;
+    };
+
+    const dragStop = () => {
+        isDragging = false; 
+        carousel.classList.remove("dragging");
+    };
+
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mousemove", dragging);
+    document.addEventListener("mouseup", dragStop);
+    
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            carousel.scrollLeft += btn.id === "left" ? 
+                -firstCardWidth : firstCardWidth;
+        });
+    });
+});
