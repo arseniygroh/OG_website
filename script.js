dfb148a84276e2e7c9af57fa227bbe89d56f4102
Vladyslav Kubrak <vladyslav.kubrak@student.griffith.ie>
2025-05-09 14:21:47 +0100

    fix script

--- script.js
+++ script.js
@@ -25,102 +25,104 @@ for (let i = 0; i < accordionBtns.length; i++) {
     })
 }
 
+if (window.location.href.includes("teams") || window.location.href.includes("index")) {
 
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector(".carousel");
-    const arrowBtns = document.querySelectorAll(".carousel-action-button");
+    document.addEventListener("DOMContentLoaded", function () {
+        const carousel = document.querySelector(".carousel");
+        const arrowBtns = document.querySelectorAll(".carousel-action-button");
 
-    const firstCard = carousel.querySelector(".card");
-    const firstCardWidth = firstCard.offsetWidth;
+        const firstCard = carousel.querySelector(".card");
+        const firstCardWidth = firstCard.offsetWidth;
 
-    let isDragging = false,
-        startX,
-        startScrollLeft;
+        let isDragging = false,
+            startX,
+            startScrollLeft;
 
-    const dragStart = (e) => { 
-        isDragging = true;
-        carousel.classList.add("dragging");
-        startX = e.pageX;
-        startScrollLeft = carousel.scrollLeft;
-    };
+        const dragStart = (e) => {
+            isDragging = true;
+            carousel.classList.add("dragging");
+            startX = e.pageX;
+            startScrollLeft = carousel.scrollLeft;
+        };
 
-    const dragging = (e) => {
-        if (!isDragging) return;
-    
-        const newScrollLeft = startScrollLeft - (e.pageX - startX);
+        const dragging = (e) => {
+            if (!isDragging) return;
 
-        if (newScrollLeft <= 0 || newScrollLeft >= 
-            carousel.scrollWidth - carousel.offsetWidth) {
+            const newScrollLeft = startScrollLeft - (e.pageX - startX);
+
+            if (newScrollLeft <= 0 || newScrollLeft >=
+                carousel.scrollWidth - carousel.offsetWidth) {
+                isDragging = false;
+                return;
+            }
+
+            carousel.scrollLeft = newScrollLeft;
+        };
+
+        const dragStop = () => {
             isDragging = false;
-            return;
-        }
+            carousel.classList.remove("dragging");
+        };
 
-        carousel.scrollLeft = newScrollLeft;
-    };
+        carousel.addEventListener("mousedown", dragStart);
+        carousel.addEventListener("mousemove", dragging);
+        document.addEventListener("mouseup", dragStop);
 
-    const dragStop = () => {
-        isDragging = false; 
-        carousel.classList.remove("dragging");
-    };
-
-    carousel.addEventListener("mousedown", dragStart);
-    carousel.addEventListener("mousemove", dragging);
-    document.addEventListener("mouseup", dragStop);
-    
-    arrowBtns.forEach(btn => {
-        btn.addEventListener("click", () => {
-            carousel.scrollLeft += btn.id === "left" ? 
-                -firstCardWidth : firstCardWidth;
+        arrowBtns.forEach(btn => {
+            btn.addEventListener("click", () => {
+                carousel.scrollLeft += btn.id === "left" ?
+                    -firstCardWidth : firstCardWidth;
+            });
         });
     });
-});
 
 
 
-const scrollers = document.querySelectorAll(".front-partners-logos-slider");
+    const scrollers = document.querySelectorAll(".front-partners-logos-slider");
 
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    addAnimation();
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+        addAnimation();
+    }
+
+    function addAnimation() {
+        scrollers.forEach((scroller) => {
+            scroller.setAttribute("data-animated", true);
+
+            const scrollerInner = scroller.querySelector(".front-partners-slider-track");
+            const scrollerContent = Array.from(scrollerInner.children);
+
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                duplicatedItem.setAttribute("aria-hidden", true);
+                scrollerInner.appendChild(duplicatedItem);
+            });
+        });
+    }
+
+
+
+    const players_pr = document.querySelectorAll(".ogroster-player-block");
+    const players = document.querySelectorAll(".ogroster-player-content");
+
+    players_pr[0].classList.add("active_pr-show")
+    players[0].classList.add("active_player-show")
+
+    players_pr.forEach((prElement, index) => {
+        prElement.addEventListener('click', () => {
+            players_pr.forEach(el => el.classList.remove("active_pr-show"));
+            players.forEach(el => el.classList.remove("active_player-show"));
+
+            prElement.classList.add("active_pr-show");
+            players[index].classList.add("active_player-show");
+        });
+    });
 }
-
-function addAnimation() {
-    scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-
-    const scrollerInner = scroller.querySelector(".front-partners-slider-track");
-    const scrollerContent = Array.from(scrollerInner.children);
-
-    scrollerContent.forEach((item) => {
-        const duplicatedItem = item.cloneNode(true);
-        duplicatedItem.setAttribute("aria-hidden", true);
-        scrollerInner.appendChild(duplicatedItem);
-    });
-    });
-}
-
-
-
-const players_pr = document.querySelectorAll(".ogroster-player-block");
-const players = document.querySelectorAll(".ogroster-player-content");
-
-players_pr[0].classList.add("active_pr-show")
-players[0].classList.add("active_player-show")
-
-players_pr.forEach((prElement, index) => {
-    prElement.addEventListener('click', () => {
-        players_pr.forEach(el => el.classList.remove("active_pr-show"));
-        players.forEach(el => el.classList.remove("active_player-show"));
-
-        prElement.classList.add("active_pr-show");
-        players[index].classList.add("active_player-show");
-    });
-});
 
 if (window.location.href.includes("history_cs")) {
     const years = document.querySelectorAll("#history_years a");
     // console.log(years);
     window.addEventListener("scroll",()=>{
-        console.log(window.scrollY);
+        // console.log(window.scrollY);
         years.forEach(element => {
            element.classList.remove("year_selected") 
         });
@@ -151,7 +153,7 @@ else if (window.location.href.includes("history")) {
     const years = document.querySelectorAll("#history_years a");
     // console.log(years);
     window.addEventListener("scroll",()=>{
-        console.log(window.scrollY);
+        // console.log(window.scrollY);
         years.forEach(element => {
            element.classList.remove("year_selected") 
         });
